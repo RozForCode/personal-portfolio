0de728677aaafbfc0378470b69b0e4443f5f1d8b
RozForCode <142087886+RozForCode@users.noreply.github.com>
2024-01-12 02:23:07 +0000

    finished contact section
    So done with styling and js of contact used emailjs library to do the background email work

--- script.js
+++ script.js
@@ -31,7 +31,26 @@ const shadowHeader = () =>{
   this.scrollY >= 50 ? header.classList.add('shadow-Header') : header.classList.remove('shadow-Header');
 }
 window.addEventListener('scroll', shadowHeader);
+
+
 /*--------EMAIL JS-------*/
+const contactForm = document.getElementById('contact-form');
+const contactMessage = document.getElementById('contact-message');
+const sendEmail = (e) =>{
+  // console.log('sending email...');
+  e.preventDefault();
+  //serviceId templateId #form publicKey
+  emailjs.sendForm('service_5pwlp9d','template_ho2gllc','#contact-form','6N5I1L_vdHHOzRCqX').then(() =>{
+    //show sent message
+    contactMessage.textContent = "Message successfully sent!"
+    //remove msg after 5 secs
+    setTimeout( () =>{
+      contactMessage.textContent = ""
+    },5000);
+  }
+}
+contactForm.addEventListener('submit', sendEmail);
+
 
 /*------SHOW SCROOL UP------*/
 
