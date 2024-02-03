a7664563c2307ef98cd38040034b309d84bec681
RozForCode <142087886+RozForCode@users.noreply.github.com>
2024-02-03 02:57:45 +0000

    Ready to deploy
    Added scrollReveal animation and fixed some things. The project is now finished!!

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 /*---------  MENU  ---------*/
-    /*---------SHOW MENU---------*/
-    const navMenu = document.getElementById('nav-menu'),
-          navToggle = document.getElementById('nav-toggle'),
-          navClose = document.getElementById('nav-close');
-    if(navToggle){
-      navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu');
-      });
-    }
-    /*------MENU HIDDEN---*/
-    if(navClose){
-      navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu');
-      });
-    }
-    
-    /*------REMOVING MENU MOBILE---*/
-  const navLink = document.querySelectorAll('.nav__link');
-  const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu');
-    //clicking on each_navlink, removes the show-menu class
+/*---------SHOW MENU---------*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+/*------MENU HIDDEN---*/
+if (navClose) {
+  navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu');
-  }
-  navLink.forEach(n => n.addEventListener('click', linkAction));
+  });
+}
+
+/*------REMOVING MENU MOBILE---*/
+const navLink = document.querySelectorAll('.nav__link');
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  //clicking on each_navlink, removes the show-menu class
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*-----------SHADOW HEADER------*/
-const shadowHeader = () =>{
+const shadowHeader = () => {
   const header = document.getElementById('header');
   //when the scroll is greater than 50 viewport height,add scrollHeader class to header tag
   this.scrollY >= 50 ? header.classList.add('shadow-Header') : header.classList.remove('shadow-Header');
@@ -36,17 +36,17 @@ window.addEventListener('scroll', shadowHeader);
 /*--------EMAIL JS-------*/
 const contactForm = document.getElementById('contact-form');
 const contactMessage = document.getElementById('contact-message');
-const sendEmail = (e) =>{
+const sendEmail = (e) => {
   // console.log('sending email...');
   e.preventDefault();
   //serviceId templateId #form publicKey
-  emailjs.sendForm('service_5pwlp9d','template_ho2gllc','#contact-form','6N5I1L_vdHHOzRCqX').then(() =>{
+  emailjs.sendForm('service_5pwlp9d', 'template_ho2gllc', '#contact-form', '6N5I1L_vdHHOzRCqX').then(() => {
     //show sent message
     contactMessage.textContent = "Message successfully sent!"
     //remove msg after 5 secs
-    setTimeout( () =>{
+    setTimeout(() => {
       contactMessage.textContent = ""
-    },5000);
+    }, 5000);
   })
 }
 contactForm.addEventListener('submit', sendEmail);
@@ -56,7 +56,7 @@ contactForm.addEventListener('submit', sendEmail);
 
 const scrollUp = () => {
   const scrollUpButton = document.getElementById("scroll-up");
-  window.scrollY >=350 ? scrollUpButton.classList.add('show-scroll') : scrollUpButton.classList.remove('show-scroll');
+  window.scrollY >= 350 ? scrollUpButton.classList.add('show-scroll') : scrollUpButton.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp);
 
@@ -66,14 +66,14 @@ window.addEventListener('scroll', scrollUp);
 const sections = document.querySelectorAll('section[id]');
 const scrollActice = () => {
   const scrollDown = window.scrollY;
-  sections.forEach(current =>{
+  sections.forEach(current => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 58;
     const sectionId = current.getAttribute('id');
     const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
-    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
       sectionsClass.classList.add('active-link');
-    }else{
+    } else {
       sectionsClass.classList.remove('active-link');
     }
   });
@@ -106,7 +106,19 @@ window.addEventListener('scroll', scrollActice);
 //   localStorage.setItem('selected-icon', currentIcon());
 // })
 var icon = document.getElementById("theme-button");
-icon.onclick = function(){
+icon.onclick = function() {
   document.body.classList.toggle("dark-theme");
 }
 /*------SCROLL REVEAL ANIMATION------*/
+const sr = ScrollReveal(
+  {
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay:350,
+    // reset:true//animation repeats
+  }
+)
+sr.reveal(`.home__perfil ,.about__img ,.contact__mail`, { origin: 'right' });
+sr.reveal(`.home__name , .home__info,.about__container ,.contact__social ,.contact__data`, { origin: 'left' });
+sr.reveal(`.services__card ,.projects__card` , { interval:100 });
